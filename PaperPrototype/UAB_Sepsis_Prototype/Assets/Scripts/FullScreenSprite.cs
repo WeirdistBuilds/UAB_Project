609ef9543db1509d1b80cca150065f899e388c7e
Unknown <kylehinckley@hotmail.com>
2019-08-02 00:36:37 -0600

    UAB Prototype HUD Layout 2

---
+++ PaperPrototype/UAB_Sepsis_Prototype/Assets/Scripts/FullScreenSprite.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FullScreenSprite : MonoBehaviour
+{
+    private float cameraHeight;
+    private Vector2 cameraSize;
+    private Vector2 imageSize;
+    private Vector2 scale;
+    private RectTransform rectTransform;
+    
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        cameraHeight = Camera.main.orthographicSize * 2;
+        cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+        imageSize = rectTransform.sizeDelta;
+        
+        scale = rectTransform.localScale;
+        if (cameraSize.x >= cameraSize.y)
+        {
+            scale *= cameraSize.x / imageSize.x;
+        } 
+        else
+        {
+            scale *= cameraSize.y / imageSize.y;
+        }
+    }
+}
