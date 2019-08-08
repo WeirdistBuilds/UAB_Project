84748e78cc25b6c98e259ca2116138aac44dd096
Unknown <kylehinckley@hotmail.com>
2019-08-08 08:38:06 -0600

    UAB Prototype Meds and Water Buttons

---
+++ PaperPrototype/UAB_Sepsis_Prototype/Assets/Scripts/AdministerMedsButton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdministerMedsButton : MonoBehaviour
+{
+
+    public SOBool Screen2, Screen3, PerformedSepsis, GaveMeds1, GaveMeds2, WantedMeds1;
+    public Text InfoText;
+
+    public void Call()
+    {
+        if (!Screen2.Value && !Screen3.Value)
+        {
+            if (!GaveMeds1.Value)
+            {
+                InfoText.text = "You gave the patient 5 mg Amlodipine";
+                GaveMeds1.Value = true;
+            }
+            else
+            {
+                InfoText.text = "The patient doesn't need any more medication";
+            }
+        }
+        else if (Screen2.Value && !Screen3.Value)
+        {
+            if (WantedMeds1.Value && !GaveMeds2.Value)
+            {
+                InfoText.text = "You gave the patient 500mg Tylenol";
+                GaveMeds2.Value = true;
+            }
+            else
+            {
+                InfoText.text = "The patient doesn't need any more medication";
+            }
+        }
+        else if (Screen3.Value && !PerformedSepsis.Value)
+        {
+            InfoText.text = "The patient is unresponsive";
+        }
+        else if (Screen3.Value && PerformedSepsis.Value)
+        {
+            InfoText.text = "The patient doesn't need any more medication";
+        }
+    }
+}
---
+++ PaperPrototype/UAB_Sepsis_Prototype/Assets/Scripts/BringWaterButton.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BringWaterButton : MonoBehaviour
+{
+
+    public SOBool Screen2, Screen3, PerformedSepsis, WantedWater1, WantedWater2, GaveWater1, GaveWater2;
+    public Text InfoText;
+
+    public void Call()
+    {
+        if (!Screen2.Value && !Screen3.Value)
+        {
+            if (!WantedWater1.Value)
+            {
+                InfoText.text = "Check to see if they want water first";
+            }
+            else if (WantedWater1.Value && !GaveWater1.Value)
+            {
+                InfoText.text = "You gave the patient a glass of water";
+                GaveWater1.Value = true;
+            }
+            else
+            {
+                InfoText.text = "The patient already has water";
+            }
+        }
+        else if (Screen2.Value && !Screen3.Value)
+        {
+            InfoText.text = "Check to see if they want water first";
+        }
+        else if (Screen3.Value && !PerformedSepsis.Value)
+        {
+            InfoText.text = "The patient is unresponsive";
+        }
+        else if (Screen3.Value && PerformedSepsis.Value)
+        {
+            if (!WantedWater2.Value)
+            {
+                InfoText.text = "Check to see if they want water first";
+            }
+            else if (WantedWater2.Value && !GaveWater2.Value)
+            {
+                InfoText.text = "You gave the patient a glass of water";
+                GaveWater2.Value = true;
+            }
+            else
+            {
+                InfoText.text = "The patient already has water";
+            }
+        }
+    }
+}
