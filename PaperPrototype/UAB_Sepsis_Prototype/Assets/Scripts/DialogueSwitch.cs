1678f1701602e5b65f16703fed3540943c49b150
Unknown <kylehinckley@hotmail.com>
2019-08-08 00:59:20 -0600

    UAB Prototype Code Buttons, Score Bools, Dialogue Work

--- PaperPrototype/UAB_Sepsis_Prototype/Assets/Scripts/DialogueSwitch.cs
+++ PaperPrototype/UAB_Sepsis_Prototype/Assets/Scripts/DialogueSwitch.cs
@@ -3,30 +3,29 @@
 public class DialogueSwitch : MonoBehaviour
 {
     public GameObject Dialogue1, Dialogue2Good, Dialogue2Bad, Dialogue3Good, Dialogue3Bad;
-    public SOBool Screen2, Screen3, CalledPhysician, PerformedQSepsis;
+    public SOBool Screen2, Screen3, CalledPhysician, PerformedSepsis;
     
     public void Call()
     {
-        if (Screen3 && PerformedQSepsis)
+        if (Screen3.Value && PerformedSepsis.Value)
         {
-            Dialogue3Good.SetActive(!Dialogue3Good.activeInHierarchy);
+            Dialogue3Good.SetActive(true);
         }
-        else if (Screen3 && CalledPhysician)
+        else if (Screen3.Value && !PerformedSepsis.Value)
         {
-            Dialogue3Bad.SetActive(!Dialogue3Bad.activeInHierarchy);
+            Dialogue3Bad.SetActive(true);
         }
-        else if (Screen2 && !CalledPhysician)
+        else if (Screen2.Value && !CalledPhysician.Value)
         {
-            Dialogue2Good.SetActive(!Dialogue2Good.activeInHierarchy);
+            Dialogue2Good.SetActive(true);
         }
-        else if (Screen2 && CalledPhysician)
+        else if (Screen2.Value && CalledPhysician.Value)
         {
-            Dialogue2Bad.SetActive(!Dialogue2Bad.activeInHierarchy);
+            Dialogue2Bad.SetActive(true);
         }
         else
         {
-            Dialogue1.SetActive(!Dialogue1.activeInHierarchy);
+            Dialogue1.SetActive(true);
         }
     }
-    
 }
